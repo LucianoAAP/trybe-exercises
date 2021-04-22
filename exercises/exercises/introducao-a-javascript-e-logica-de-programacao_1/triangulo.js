dff229a7bbb0b05c24ac7d0de3b3654c3789b9f6
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-04-22 18:31:53 -0300

    adiciona exercicio de triangulo

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
@@ -0,0 +1,10 @@
+let x=70;
+let y=50;
+let z=60;
+if (x>=180 || y>=180 || z>=180) {
+    console.log ('erro');
+} else if (x+y+z==180) {
+    console.log (true);
+} else {
+    console.log (false);
+}
