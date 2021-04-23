5407edadee71be320534f3767cba27fb6698e2e6
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-04-23 15:13:50 -0300

    adiciona exercicio de for e for of

---
+++ exercises/bloco-4/introducao-a-javascript-e-logica-de-programacao_2/for.js
@@ -0,0 +1,8 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i=0; i<4; i++) {
+    console.log(groceryList[i]);
+}
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nome of names) {
+    console.log(nome);
+}
