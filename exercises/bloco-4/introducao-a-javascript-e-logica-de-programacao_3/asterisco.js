0664be01ec359167aad271ecf54a045facbba271
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-04-26 16:51:05 -0300

    adiciona exercicio de triangulo invertido

--- exercises/bloco-4/introducao-a-javascript-e-logica-de-programacao_3/asterisco.js
+++ exercises/bloco-4/introducao-a-javascript-e-logica-de-programacao_3/asterisco.js
@@ -3,12 +3,22 @@ let quadrado = '';
 for (let index=1; index<=n; index++) {
     quadrado=quadrado+'*';
 }
-for (let index=0; index<quadrado.length; index++) {
+for (let index=1; index<=n; index++) {
     console.log (quadrado);
 }
-console.log('========================')
+console.log('========================');
 let triangulo='';
 for (let index = 1; index<=n; index++) {
     triangulo=triangulo+'*';
     console.log(triangulo);
+}
+console.log('========================');
+let invertido ='';
+let index2=0;
+let espaco=' ';
+let asterisco='*';
+for (let index=1; index<=n; index++) {
+    invertido=invertido+espaco.repeat(n-index)+asterisco.repeat(index);
+    console.log(invertido);
+    invertido='';
 }
