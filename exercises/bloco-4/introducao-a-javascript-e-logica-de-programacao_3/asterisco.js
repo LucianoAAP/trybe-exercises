25795a560535a5f52b08f8579ef3b925e836b790
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-04-26 15:33:42 -0300

    adiciona exercicio de triangulo

---
+++ exercises/bloco-4/introducao-a-javascript-e-logica-de-programacao_3/asterisco.js
@@ -0,0 +1,14 @@
+let n=5;
+let quadrado = '';
+for (let index=1; index<=n; index++) {
+    quadrado=quadrado+'*';
+}
+for (let index=0; index<quadrado.length; index++) {
+    console.log (quadrado);
+}
+console.log('========================')
+let triangulo='';
+for (let index = 1; index<=n; index++) {
+    triangulo=triangulo+'*';
+    console.log(triangulo);
+}
