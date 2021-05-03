75a6a45afa13ee40cd1e43ed3224526e68713cda
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-03 20:42:31 -0300

    adiciona exercicios do estudo

--- exercises/bloco-5/javascript-dom-eventos-e-web-storage_3/main.js
+++ exercises/bloco-5/javascript-dom-eventos-e-web-storage_3/main.js
@@ -5,19 +5,55 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+1. Copie esse arquivo e edite apenas ele;
+
+2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+
+4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+4.1. Que tal redirecionar para seu portifólio?
+
+5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
+
+divUm.addEventListener('click', recebeClasse);
+divDois.addEventListener('click', recebeClasse);
+divTres.addEventListener('click', recebeClasse);
+
+input.addEventListener('input', alteraTexto);
+
+myWebpage.addEventListener('dblclick', link);
+myWebpage.addEventListener('mouseover', alteraCor);
+myWebpage.addEventListener('mouseout', removeCor);
+
+function recebeClasse (event) {
+    let elementoClasse = document.querySelector('.tech');
+    elementoClasse.classList.remove('tech');
+    event.target.classList.add('tech');
+};
+
+function alteraTexto (event) {
+    let elementoClasse = document.querySelector('.tech');
+    elementoClasse.innerText = event.target.value;
+};
+
+function link () {
+    window.open('https://lucianoaap.github.io/');
+};
+
+function alteraCor (event) {
+    event.target.style.color = 'red';
+};
+function removeCor (event) {
+    event.target.style.color = 'unset';
+};
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
@@ -27,6 +63,8 @@ function resetText(event) {
 }
 
 divUm.addEventListener('dblclick', resetText);
+divDois.addEventListener('dblclick', resetText);
+divTres.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
