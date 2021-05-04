eca92c9e97f1f3084d6d11b7f568215f8f916451
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-03 22:56:46 -0300

    adiciona exercicios de fixaçao ja com muita coisa que eu esqueci de commitar

---
+++ exercises/bloco-5/javascript-dom-eventos-e-web-storage_3/script2.js
@@ -0,0 +1,54 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let index in dezDaysList) {
+      let dia = document.createElement('li')
+      dia.innerText = dezDaysList[index];
+      dia.classList.add('day');
+      if (dia.innerText === '24' || dia.innerText === '25' || dia.innerText === '31') {
+          dia.classList.add('holiday');
+      };
+      if (dia.innerText === '4' || dia.innerText === '11' || dia.innerText === '18' || dia.innerText === '25') {
+          dia.classList.add('friday');
+      }
+      document.querySelector('#days').appendChild(dia);
+  };
+  function addHoliday (btn) {
+      let botao = document.createElement('button')
+      botao.id = "btn-holiday";
+      botao.innerHTML = btn;
+      document.querySelector('.buttons-container').appendChild(botao);
+  };
+  addHoliday('Feriados');
+  
+  document.querySelector('#btn-holiday').addEventListener('click', holidayColor);
+  function holidayColor () {
+    let holidays = document.querySelectorAll('.holiday');
+      for (let index = 0; index < holidays.length; index += 1) {
+        if (holidays[index].style.backgroundColor !== 'red') {
+            holidays[index].style.backgroundColor = 'red';
+        } else {
+            holidays[index].style.backgroundColor = 'rgb(238,238,238)';
+        }
+      };
+  };
+  function sextou(btn) {
+      let sexta = document.createElement('button');
+      sexta.innerHTML = btn;
+      document.querySelector('.buttons-container').appendChild(sexta);
+  }
+  sextou('Sexta-feira');
