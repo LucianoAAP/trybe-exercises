4b286d250fb8b77b21f8557066a47b1070b1bd48
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-19 17:56:36 -0300

    conclui questao 1 do exercicio 2

--- exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
+++ exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
@@ -1,8 +1,12 @@
-const fatorial = numero => {
+const mult = numero => {
   for (let index = numero - 1; index > 0; index -= 1) {
     numero = numero * index;
   }
   return numero;
 }
+
+const fatorial = numero => (
+  numero === 0 ? 1 : mult(numero)
+);
 
 console.log(fatorial(4));
