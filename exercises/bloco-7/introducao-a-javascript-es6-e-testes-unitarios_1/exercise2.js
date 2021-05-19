fcaa1590c6aacf04591d45b6bdb29ff10b0ac4f4
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-19 18:57:21 -0300

    adiciona questao 2 do exercicio 2

--- exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
+++ exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
@@ -10,3 +10,13 @@ const fatorial = numero => (
 );
 
 console.log(fatorial(4));
+
+const longestWord = phrase => {
+  const splittedPhrase = phrase.split(' ');
+  let longest = splittedPhrase[0];
+  for (let index = 1; index < splittedPhrase.length; index += 1) {
+    splittedPhrase[index].length > splittedPhrase[index - 1].length ? longest = splittedPhrase[index] : longest = splittedPhrase[index - 1];
+  }
+  return longest;
+}
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")); // retorna 'aconteceu'
