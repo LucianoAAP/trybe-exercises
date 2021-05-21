d82db07cc94c5ead3d2a5fa901ced82a914c9b6a
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-21 18:02:34 -0300

    adiciona parte 1

--- exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
+++ exercises/bloco-7/introducao-a-javascript-es6-e-testes-unitarios_3/parte1.js
@@ -21,4 +21,7 @@ assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 
 // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+
+assert.strictEqual(sum(4, '5'), 9);
+
 // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
