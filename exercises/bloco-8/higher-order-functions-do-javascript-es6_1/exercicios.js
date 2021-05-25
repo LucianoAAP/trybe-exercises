3f94d81efde2e4090c3b682363ad2cff3fd2b915
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-25 17:56:46 -0300

    adiciona exercicio 3

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_1/exercicios.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_1/exercicios.js
@@ -11,7 +11,7 @@ const newEmployee = (nome) => {
   let objeto = {};
   objeto.nomeCompleto = nome;
   objeto.email = `${nome.toLowerCase().split(' ').join('_')}@gmail.com`;
-  return objeto; 
+  return objeto;
 };
 
 console.log(newEmployees(newEmployee));
@@ -25,8 +25,29 @@ const verificar = (numeroSorteado, numero) => {
 }
 
 const sorteio = (numero, callback) => {
-  const numeroSorteado = Math.round(Math.random()*5);
+  const numeroSorteado = Math.round(Math.random() * 5);
   return callback(numeroSorteado, numero);
 }
 
 console.log(sorteio(4, verificar));
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const verificarRespostas = (respostaCorreta, respostaEstudante) => {
+  if (respostaEstudante === 'N.A') {
+    return 0;
+  } else if (respostaCorreta === respostaEstudante) {
+    return 1;
+  } else {
+    return -0.5;
+  }
+};
+const gerarNota = (rightAnswers, studentAnswers, callback) => {
+  let nota = 0;
+  for (let index = 0; index < studentAnswers.length; index += 1) {
+    nota += callback(rightAnswers[index], studentAnswers[index]);
+  }
+  return nota;
+}
+
+console.log(gerarNota(rightAnswers, studentAnswers, verificarRespostas));
