5b8f9e970f26c0a894c4a2b19c20e9175c4e6648
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-26 16:40:30 -0300

    segundo ex conteudo

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios-conteudo.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios-conteudo.js
@@ -11,3 +11,12 @@ const showEmailList = (email) => {
 
 // Adicione seu código aqui
 emailListInData.forEach(showEmailList);
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => number % 2 === 0 && number % 5 === 0);
+}
+
+console.log(findDivisibleBy3And5());
