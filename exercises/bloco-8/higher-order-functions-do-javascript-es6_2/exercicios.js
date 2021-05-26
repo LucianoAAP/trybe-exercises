36e8ce4ce78bdbac4fe43dd761f76d662eda88ef
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-26 18:14:12 -0300

    6o ex

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios.js
@@ -178,3 +178,14 @@ function everyoneWasBornOnSecXX() {
 }
 
 assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult3);
+
+// Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário. 
+
+const expectedResult4 = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult4);
