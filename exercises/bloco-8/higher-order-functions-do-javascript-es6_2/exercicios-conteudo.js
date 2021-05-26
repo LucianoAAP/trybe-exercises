92664680165dc097646be2ad03bc4cdb822a2086
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-26 16:47:50 -0300

    4o ex conteudo

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios-conteudo.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_2/exercicios-conteudo.js
@@ -29,3 +29,16 @@ const findNameWithFiveLetters = () => {
 }
 
 console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find((musica) => musica.id === '31031685');
+}
+
+console.log(findMusic('31031685'))
