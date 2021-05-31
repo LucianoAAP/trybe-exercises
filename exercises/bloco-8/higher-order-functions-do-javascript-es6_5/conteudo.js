5f3f0044882456675c67bdcaf1f983131c2e3ec3
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-31 15:43:50 -0300

    quarto ex conteudo

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_5/conteudo.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_5/conteudo.js
@@ -31,3 +31,12 @@ console.log(comida, animal, bebida); // arroz gato água
 
 [comida, animal, bebida] = [bebida, comida, animal];
 console.log([comida, animal, bebida]);
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares;
+console.log(numerosPares);
