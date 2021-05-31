7f3446353bb5d3cc7aa9477f153b9af93b1184ea
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-31 15:38:44 -0300

    terceiro ex conteudo

--- exercises/bloco-8/higher-order-functions-do-javascript-es6_5/conteudo.js
+++ exercises/bloco-8/higher-order-functions-do-javascript-es6_5/conteudo.js
@@ -20,3 +20,14 @@ saudacoes[1](saudacoes[0]); // Olá
 const [ola, funcaoSaudaçao] = saudacoes;
 
 funcaoSaudaçao(ola);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log([comida, animal, bebida]);
