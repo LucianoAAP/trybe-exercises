bc28d897835f93408cc956bc3e6a8745e0fb4274
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-07 19:12:21 -0300

    adiciona ex 6

--- exercises/bloco-9/exercicios.js
+++ exercises/bloco-9/exercicios.js
@@ -51,19 +51,33 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-
-const sendMarsTemperature = (callback) => {
-  const delay = messageDelay();
-  const temperature = getMarsTemperature();
-  setTimeout(() => callback(temperature), delay);
-};
-
-// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
-
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = (onSuccess, onFailure) => {
+  const delay = messageDelay();
+  const temperature = getMarsTemperature();
+  const successTest = Math.random();
+  if (successTest >= 0.6) {
+    setTimeout(() => onSuccess(temperature), delay);
+  } else {
+    const errorReason = 'Robot is busy';
+    setTimeout(() => onFailure(errorReason), delay);
+  }
+};
+
+// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+// sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
