3509ce6236a566dfad230ba95985f94f4c3d7130
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-07 18:54:43 -0300

    adiciona ex 5

--- exercises/bloco-9/exercicios.js
+++ exercises/bloco-9/exercicios.js
@@ -53,9 +53,17 @@ const getMarsTemperature = () => {
 
 // crie a função sendMarsTemperature abaixo
 
-const sendMarsTemperature = () => {
+const sendMarsTemperature = (callback) => {
   const delay = messageDelay();
-  setTimeout(() => console.log(getMarsTemperature()), delay);
+  const temperature = getMarsTemperature();
+  setTimeout(() => callback(temperature), delay);
 };
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
