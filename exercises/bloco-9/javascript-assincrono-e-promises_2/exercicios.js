473890be484d8bc8950036bbb8380d95cf9b930d
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-08 19:27:05 -0300

    adiciona ex 3 e 4

--- exercises/bloco-9/javascript-assincrono-e-promises_2/exercicios.js
+++ exercises/bloco-9/javascript-assincrono-e-promises_2/exercicios.js
@@ -5,10 +5,10 @@ for (let index = 1; index <= 10; index += 1) {
 }
   const soma = array.reduce((accumulator, current) => accumulator += current, 0);
   if (soma < 8000) {
-    resolve();
+    resolve([soma / 2, soma / 3, soma / 5, soma / 10]);
   } else {
     reject();
   }
 })
 .then(() => console.log('Promise resolvida'))
-.catch(() => console.log('Promise rejeitada'));
+.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
