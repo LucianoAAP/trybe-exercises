7f1854d112a8e777eff6e6af309270e706de8a08
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-08 19:49:41 -0300

    adiciona ex 5

--- exercises/bloco-9/javascript-assincrono-e-promises_2/exercicios.js
+++ exercises/bloco-9/javascript-assincrono-e-promises_2/exercicios.js
@@ -1,8 +1,8 @@
 const getNumber = new Promise((resolve, reject) => {
   let array = [];
-for (let index = 1; index <= 10; index += 1) {
-  array.push(Math.floor(Math.random() * 50) ** 2);
-}
+  for (let index = 1; index <= 10; index += 1) {
+    array.push(Math.floor(Math.random() * 50) ** 2);
+  }
   const soma = array.reduce((accumulator, current) => accumulator += current, 0);
   if (soma < 8000) {
     resolve([soma / 2, soma / 3, soma / 5, soma / 10]);
@@ -10,5 +10,13 @@ for (let index = 1; index <= 10; index += 1) {
     reject();
   }
 })
-.then(() => console.log('Promise resolvida'))
+.then((array) => {
+  console.log('Promise resolvida');
+  return array;
+})
+.then((array) => {
+  const soma = array.reduce((accumulator, current) => accumulator += current, 0);
+  console.log(soma);
+  return soma;
+})
 .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
