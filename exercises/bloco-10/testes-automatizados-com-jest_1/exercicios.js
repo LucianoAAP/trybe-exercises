27af2b4fb539629ba1b49323954afc7637994711
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-14 19:38:12 -0300

    adiciona exercicio 3

--- exercises/bloco-10/testes-automatizados-com-jest_1/exercicios.js
+++ exercises/bloco-10/testes-automatizados-com-jest_1/exercicios.js
@@ -5,6 +5,21 @@ const techList = (listaDeTecnologias, nome) => {
   return listaDeTecnologias.map((tecnologia) => ({ tech: tecnologia, name: nome})).sort((objA, objB) => objA.tech > objB.tech ? 1: -1);
 }
 
+const hydrate = (numerobebida) => {
+  const splitted = numerobebida.match(/\d+/g).map(Number);
+  number = splitted.reduce((accumulator, current) => {
+    if (typeof parseInt(current) === "number") {
+      accumulator += current;
+    }
+    return accumulator
+  }, 0);
+  if (number === 0 || number === 1) {
+    return `${number} copo de água`;
+  }
+  return `${number} copos de água`;
+}
+
 module.exports = {
-  techList
+  techList,
+  hydrate
 }
