884d1044b98250eeb042297068f6c7eac72001c7
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-15 18:51:03 -0300

    adiciona ex 2

--- exercises/bloco-10/testes-automatizados-com-jest_2/exercicios.test.js
+++ exercises/bloco-10/testes-automatizados-com-jest_2/exercicios.test.js
@@ -5,7 +5,7 @@ const uppercase = (str, callback) => {
 };
 
 describe('Testa uppercase', () => {
-  it('should be upper case', (done) => {
+  test('should be upper case', (done) => {
     const callback = (param) => {
       setTimeout(() => {
         expect(param).toBe('TESTE');
@@ -15,3 +15,27 @@ describe('Testa uppercase', () => {
     uppercase('teste', callback);
   });
 });
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+  };
+  
+  const findUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        if (users[id]) {
+          return resolve(users[id]);
+        };
+  
+        return reject({ error: 'User with ' + id + ' not found.' });
+    });
+  };
+  
+  const getUserName = (userID) => {
+    return findUserById(userID).then(user => user.name);
+  };
+
+  describe('Testa getUserName', () => {
+    it('should work', () => getUserName(4).then((name) => expect(name).toBe('Mark')));
+    it('should not work', () => getUserName(3).catch((name) => expect(name).toEqual({ error: 'User with 3 not found.'})));
+  });
