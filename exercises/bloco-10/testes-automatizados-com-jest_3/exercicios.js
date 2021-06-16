03622909ae08cd2701da327cef98aa93db18b6bc
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-16 20:01:41 -0300

    adiciona ex 4

--- exercises/bloco-10/testes-automatizados-com-jest_3/exercicios.js
+++ exercises/bloco-10/testes-automatizados-com-jest_3/exercicios.js
@@ -1,5 +1,14 @@
 const randomNumber = () => Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 
+const caixaAlta = (str) => str.toUpperCase();
+
+const firstLetter = (str) => str[0];
+
+const concat = (str1, str2) => `${str1}${str2}`;
+
 module.exports = {
-  randomNumber
+  randomNumber,
+  caixaAlta,
+  firstLetter,
+  concat
 }
