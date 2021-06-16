bd7d1383e6ca8040915b3957ea953ce208f600f0
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-16 16:34:57 -0300

    adiciona ex 2 conteudo

--- exercises/bloco-10/testes-automatizados-com-jest_3/math.test.js
+++ exercises/bloco-10/testes-automatizados-com-jest_3/math.test.js
@@ -1,9 +1,19 @@
+const { expect } = require('@jest/globals');
 const math = require('./math.js');
 
 describe('Trabalha subtrair', () => {
   test('Testa e mocka subtrair', () => {
     math.subtrair = jest.fn();
-    math.subtrair(1, 2);
+    math.subtrair();
     expect(math.subtrair).toHaveBeenCalled();
+  });
+});
+
+describe('Trabalha multiplicar', () => {
+  test('Testa e mocka multiplicar', () => {
+    math.multiplicar = jest.fn().mockReturnValue(10);
+    math.multiplicar();
+    expect(math.multiplicar).toHaveBeenCalled();
+    expect(math.multiplicar()).toBe(10);
   });
 });
