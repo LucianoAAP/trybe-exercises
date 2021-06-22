37ba889f4dae2716f4081dfad67ce3c225f45845
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-22 17:13:32 -0300

    2o ex conteudo

--- exercises/bloco-11/introducao-a-react_2/fixation-exercises-11-2/src/App.js
+++ exercises/bloco-11/introducao-a-react_2/fixation-exercises-11-2/src/App.js
@@ -1,13 +1,49 @@
 import './App.css';
-import Image from './image.js';
-import cuteCat from './cute-cat-staring.jpg'
+import React from 'react';
+import Order from './components/Order';
+// import Image from './components/image.js';
+// import cuteCat from './components/cute-cat-staring.jpg'
 
-function App() {
-  return (
-    <div>
-      <Image source = {cuteCat} alternativeText = 'Cute cat staring' />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Image source = {cuteCat} alternativeText = 'Cute cat staring' />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         {/*  adicione os componentes aqui */}
+         <Order order = {headphone} />
+         <Order order = {energyDrink} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
