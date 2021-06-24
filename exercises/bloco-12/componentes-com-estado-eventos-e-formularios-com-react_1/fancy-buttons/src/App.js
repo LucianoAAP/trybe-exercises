bc0558dbe7bbb2485737387e1ba0749af8e35272
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-24 17:28:44 -0300

    adiciona ex 8 e 9 conteudo e corrige problema de assincronia dos ex antigos

--- exercises/bloco-12/componentes-com-estado-eventos-e-formularios-com-react_1/fancy-buttons/src/App.js
+++ exercises/bloco-12/componentes-com-estado-eventos-e-formularios-com-react_1/fancy-buttons/src/App.js
@@ -15,30 +15,39 @@ class App extends React.Component {
   }
 
   testClick1 = () => {
-    this.setState((previous, _prop) => ({
-      Teste1: previous.Teste1 + 1
-    }));
-    console.log(this.state.Teste1);
+    const botao1 = document.querySelector('#teste_1');
+    this.setState((previous, _prop) => ({ Teste1: previous.Teste1 + 1 }),
+    () => {
+      this.state.Teste1 % 2 === 0 ? botao1.style.backgroundColor = 'green' : botao1.style.backgroundColor = 'grey';
+      console.log(botao1.style.backgroundColor);
+    }
+    );
   };
   testClick2 = () => {
-    this.setState((previous, _prop) => ({
-      Teste2: previous.Teste2 + 1
-    }));
-    console.log(this.state.Teste2);
+    const botao2 = document.querySelector('#teste_2');
+    this.setState((previous, _prop) => ({ Teste2: previous.Teste2 + 1 }),
+    () => {
+      this.state.Teste2 % 2 === 0 ? botao2.style.backgroundColor = 'green' : botao2.style.backgroundColor = 'grey';
+      console.log(botao2.style.backgroundColor);
+    }
+    );
   };
   testClick3 = () => {
-    this.setState((previous, _prop) => ({
-      Teste3: previous.Teste3 + 1
-    }));
-    console.log(this.state.Teste3);
+    const botao3 = document.querySelector('#teste_3');
+    this.setState((previous, _prop) => ({ Teste3: previous.Teste3 + 1 }),
+      () => {
+        this.state.Teste3 % 2 === 0 ? botao3.style.backgroundColor = 'green' : botao3.style.backgroundColor = 'grey';
+        console.log(botao3.style.backgroundColor);
+      }
+      );
   };
 
   render() {
     return (
     <div>
-      <button onClick={this.testClick1}>Teste 1</button>
-      <button onClick={this.testClick2}>Teste 2</button>
-      <button onClick={this.testClick3}>Teste 3</button>
+      <button id='teste_1' onClick={this.testClick1}>{this.state.Teste1}</button>
+      <button id='teste_2' onClick={this.testClick2}>{this.state.Teste2}</button>
+      <button id='teste_3' onClick={this.testClick3}>{this.state.Teste3}</button>
     </div>
     );
   }
