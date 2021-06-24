72477795a80aec8b83fed0258f98b06fe99a1eba
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-24 15:40:19 -0300

    adiciona ex 3 e 4 conteudo

--- exercises/bloco-12/componentes-com-estado-eventos-e-formularios-com-react_1/fancy-buttons/src/App.js
+++ exercises/bloco-12/componentes-com-estado-eventos-e-formularios-com-react_1/fancy-buttons/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './App.css';
 
-const testClick1 = () => console.log('Teste 1');
-const testClick2 = () => console.log('Teste 2');
-const testClick3 = () => console.log('Teste 3');
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.testClick1.bind(this);
+    this.testClick2.bind(this);
+    this.testClick3.bind(this);
+  }
+
+  testClick1 = () => console.log('Teste 1');
+  testClick2 = () => console.log('Teste 2');
+  testClick3 = () => console.log('Teste 3');
+
   render() {
     return (
     <div>
-      <button onClick={testClick1}>Teste 1</button>
-      <button onClick={testClick2}>Teste 2</button>
-      <button onClick={testClick3}>Teste 3</button>
+      <button onClick={this.testClick1}>Teste 1</button>
+      <button onClick={this.testClick2}>Teste 2</button>
+      <button onClick={this.testClick3}>Teste 3</button>
     </div>
     );
   }
