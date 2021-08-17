a37fe4e0a7fc45f4e3cfd97c9b454ffcb2d25a09
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-08-17 16:46:35 -0300

    ex 1 conteudo

--- exercises/bloco-18/react-hooks-useeffect-e-hooks-customizados/exercicio1-conteudo/src/App.js
+++ exercises/bloco-18/react-hooks-useeffect-e-hooks-customizados/exercicio1-conteudo/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import useTimer from './useTimer';
 
-function App() {
+const App = () => {
+  const { timer, randomNumber, isMultiple } = useTimer();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {timer}
+      <br />
+      {randomNumber}
+      <br />
+      {isMultiple ? 'Acerto' : ''}
     </div>
   );
-}
+};
 
 export default App;
