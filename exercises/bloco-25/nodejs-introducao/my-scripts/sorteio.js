70ce83884f21736c88f376bb0787eb41d4d091a9
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-10-20 19:16:16 -0300

    ex 7

---
+++ exercises/bloco-25/nodejs-introducao/my-scripts/sorteio.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync');
+
+const generateRandomNumber = () => Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+
+const generateResult = (number) => {
+  const rightNumber = generateRandomNumber();
+  if (number === rightNumber) console.log('Parabéns, número correto!');
+  else console.log(`Opa, não foi dessa vez. O número era ${rightNumber}`);
+};
+
+const startGame = () => {
+  const number = readline.questionInt('Digite um número: ');
+  generateResult(number);
+  playAgain();
+};
+
+const playAgain = () => {
+  const answer = readline.question('Deseja jogar novemente (s/n)? ');
+  if (answer === 's') startGame();
+};
+
+startGame();
