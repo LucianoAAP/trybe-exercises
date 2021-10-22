ffaf10f04b8da36bd9b8926211cc094217de1181
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-10-22 17:17:59 -0300

    ex 4

--- exercises/bloco-25/testes-com-nodejs/tests.js
+++ exercises/bloco-25/testes-com-nodejs/tests.js
@@ -1,5 +1,8 @@
 const getPlusMinus = require('./getPlusMinus');
+const writeInFile = require('./writeInFile');
 const { expect } = require('chai');
+const fs = require('fs/promises');
+const sinon = require('sinon');
 
 describe('testa getPlusMinus', () => {
   it('testa se parâmetro é número', () => {
@@ -24,5 +27,17 @@ describe('testa getPlusMinus', () => {
     const resposta = getPlusMinus(0);
     expect(resposta).to.be.a('string');
     expect(resposta).to.be.equal('Neutro');
+  });
+});
+
+describe('testa writeInFile', () => {
+  it('testa se é string', () => {
+    const resposta = writeInFile('writtenFile.txt', 'ronaldo');
+    expect(resposta).to.be.a('string');
+  });
+
+  it('testa se é ok', () => {
+    const resposta = writeInFile('writtenFile.txt', 'ronaldo');
+    expect(resposta).to.be.equal('ok');
   });
 });
