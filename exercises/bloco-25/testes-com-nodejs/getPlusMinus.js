1bef5d7e2829f9b066839db2af7684d74f72bf07
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-10-22 17:01:14 -0300

    ex 3

--- exercises/bloco-25/testes-com-nodejs/getPlusMinus.js
+++ exercises/bloco-25/testes-com-nodejs/getPlusMinus.js
@@ -1,4 +1,5 @@
 const getPlusMinus = (num) => {
+  if (typeof num !== 'number') return 'o valor deve ser um número';
   if (num === 0) return 'Neutro';
   if (num < 0) return 'Negativo';
   return 'Positivo';
--- exercises/bloco-25/testes-com-nodejs/tests.js
+++ exercises/bloco-25/testes-com-nodejs/tests.js
@@ -2,23 +2,27 @@ const getPlusMinus = require('./getPlusMinus');
 const { expect } = require('chai');
 
 describe('testa getPlusMinus', () => {
-  it('testa se é string', () => {
-    const resposta = getPlusMinus(1);
+  it('testa se parâmetro é número', () => {
+    const resposta = getPlusMinus('1');
     expect(resposta).to.be.a('string');
+    expect(resposta).to.be.equal('o valor deve ser um número');
   });
 
   it('testa positivo', () => {
     const resposta = getPlusMinus(1);
+    expect(resposta).to.be.a('string');
     expect(resposta).to.be.equal('Positivo');
   });
 
   it('testa negativo', () => {
     const resposta = getPlusMinus(-1);
+    expect(resposta).to.be.a('string');
     expect(resposta).to.be.equal('Negativo');
   });
 
   it('testa neutro', () => {
     const resposta = getPlusMinus(0);
+    expect(resposta).to.be.a('string');
     expect(resposta).to.be.equal('Neutro');
   });
 });
