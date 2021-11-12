0df598604641e5e01f5cd4e3133e3de57f7c5a91
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-11-11 21:08:54 -0300

    ex 4 conteudo p2

--- exercises/bloco-26/arquitetura-de-software-camada-de-model/ex-conteudo/models/Book.js
+++ exercises/bloco-26/arquitetura-de-software-camada-de-model/ex-conteudo/models/Book.js
@@ -26,22 +26,22 @@ const { ObjectId } = require('mongodb');
 //     return books.map(({ id, title, author_id }) => ({ id, title, authorId: author_id }))[0];
 // };
 
+// const isValid = async (title, author_id) => {
+//     if (!title || typeof title !== 'string') return false;
+//     const [author] = await connection.execute(
+//              'SELECT * FROM model_example.authors WHERE id = ?',
+//              [author_id],
+//          );
+//     if (!author_id || author.length === 0) return false;
+//     return true;
+// };
+
 // const create = (title, author_id) => {
 //     return connection.execute(
 //         'INSERT INTO model_example.books (title, author_id) VALUES (?,?)',
 //         [title, author_id],
 //     );
 // };
-
-const isValid = async (title, author_id) => {
-    if (!title || typeof title !== 'string') return false;
-    const [author] = await connection.execute(
-             'SELECT * FROM model_example.authors WHERE id = ?',
-             [author_id],
-         );
-    if (!author_id || author.length === 0) return false;
-    return true;
-};
 
 const getAll = async () => {
     return connection()
@@ -63,16 +63,21 @@ const getById = async (id) => {
     }
     const book = await connection()
         .then((db) => db.collection('books').findOne(new ObjectId(id)));
-        if (!book) return null;
-        const { _id, title, author_id } = book;
+    if (!book) return null;
+    const { _id, title, author_id } = book;
     return { id: _id, title, authorId: author_id };
 };
 
+const isValid = async (title, author_id) => {
+    if (!title || typeof title !== 'string') return false;
+    const author = await connection()
+        .then((db) => db.collection('books').find({ author_id: Number(author_id) }).toArray());
+    if (!author_id || author.length === 0) return false;
+    return true;
+};
+
 const create = (title, author_id) => {
-    return connection.execute(
-        'INSERT INTO model_example.books (title, author_id) VALUES (?,?)',
-        [title, author_id],
-    );
+    return connection().then((db) => db.collection('books').insertOne({ title, author_id }));
 };
 
 module.exports = {
