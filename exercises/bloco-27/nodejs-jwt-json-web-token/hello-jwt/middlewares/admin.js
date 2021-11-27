f603732b3c0e1065869337ecb964375f5e756137
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-11-26 21:43:48 -0300

    ex com erro

--- exercises/bloco-27/nodejs-jwt-json-web-token/hello-jwt/middlewares/admin.js
+++ exercises/bloco-27/nodejs-jwt-json-web-token/hello-jwt/middlewares/admin.js
@@ -1,4 +1,4 @@
-module.exports = (req, res, next) => {
+module.exports = (req, _res, next) => {
   const { admin } = req.user;
   if (!admin) return next({ statusCode: 403, message: 'Restricted access' });
   return next();
