cc90f7bf6645d73feb249195faafd232559a7b83
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-16 20:26:56 -0300

    conteudo

--- exercises/bloco-30/mvc-example/models/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'lucianoaap',
-  password: 'gatouber',
-  database: 'mvc_example'});
-
-module.exports = connection;
