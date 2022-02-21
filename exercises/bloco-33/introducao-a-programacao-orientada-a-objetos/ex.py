ebc5ef59fc5db391f423f2a83e5df4ab184965e3
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-02-21 16:22:23 -0300

    ex 4

--- exercises/bloco-33/introducao-a-programacao-orientada-a-objetos/ex.py
+++ exercises/bloco-33/introducao-a-programacao-orientada-a-objetos/ex.py
@@ -22,3 +22,29 @@ class Circle:
 
     def get_perimeter(self):
         return 2 * pi * self.radius
+
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def change_price(self, new_price):
+        self.price = new_price
+
+
+class Order:
+    def __init__(self, client, products, payment_method, discount):
+        self.client = client
+        self.products = products
+        self.payment_method = payment_method
+        self.discount = discount
+
+    def get_total_price(self):
+        total_price = 0
+        for product in self.products:
+            total_price += product.price
+        return total_price
+
+    def get_final_price(self):
+        return self.get_total_price() * (1 - self.discount)
