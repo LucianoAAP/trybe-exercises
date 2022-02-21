c3d3411663dd2127ac7415808f20e20403568c63
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-02-21 16:34:14 -0300

    ex 5

--- exercises/bloco-33/introducao-a-programacao-orientada-a-objetos/ex.py
+++ exercises/bloco-33/introducao-a-programacao-orientada-a-objetos/ex.py
@@ -48,3 +48,24 @@ class Order:
 
     def get_final_price(self):
         return self.get_total_price() * (1 - self.discount)
+
+
+class Television:
+    def __init__(self, size, volume_ratio):
+        self.size = size
+        self.volume_ratio = volume_ratio
+        self.volume = 0
+        self.on: False
+        self.channel = 1
+
+    def turn_on_or_off(self):
+        self.on = not self.on
+
+    def change_channel(self, new_channel):
+        self.channel = new_channel
+
+    def raise_volume(self):
+        self.volume += self.volume_ratio
+
+    def lower_volume(self):
+        self.volume -= self.volume_ratio
